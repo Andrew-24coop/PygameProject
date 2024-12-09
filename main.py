3a1cf5ff401e1b532a2b773efc5b0d2b6b91dcaa
SpaceLink <128892453+Andrew-24coop@users.noreply.github.com>
2024-12-09 19:08:18 +0300

    planet + grid

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import random
-
 import pygame
 import numpy as np
 import noise
 
-# Настройки Pygame
 WIDTH, HEIGHT = 1000, 600
-TILE_SIZE = 1  # Размер одной "плитки" (пикселя)
+TILE_SIZE = 1
+MAIN_TILE_SIZE = 50
 
-# Цвета для отображения различных типов местности
 COLORS = {
     "ocean": (0, 0, 255),
     "sand": (194, 178, 128),
@@ -17,10 +15,10 @@ COLORS = {
     "mountain": (139, 137, 137)
 }
 
-# Функция генерации карты с использованием семени
+
 def generate_map(width, height, seed):
     world_map = np.zeros((height, width))
-    scale = 100.0  # Масштаб перлин шума
+    scale = 100.0
     for x in range(width):
         for y in range(height):
             world_map[y][x] = noise.pnoise2(x / scale,
@@ -33,7 +31,7 @@ def generate_map(width, height, seed):
                                             base=seed)  # Используем семя
     return world_map
 
-# Функция определения типа местности по высоте
+
 def get_terrain_color(height_value):
     if height_value < -0.1:
         return COLORS["ocean"]
@@ -46,14 +44,13 @@ def get_terrain_color(height_value):
     else:
         return COLORS["mountain"]
 
-# Основная функция игры
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Realistic World Generation")
     clock = pygame.time.Clock()
 
-    # Запросите у пользователя семя
     seed = random.randint(0, 255)
 
     world_map = generate_map(WIDTH // TILE_SIZE, HEIGHT // TILE_SIZE, seed)
@@ -64,17 +61,22 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        # Отрисовка карты
         for y in range(HEIGHT // TILE_SIZE):
             for x in range(WIDTH // TILE_SIZE):
                 height_value = world_map[y][x]
                 color = get_terrain_color(height_value)
                 pygame.draw.rect(screen, color, (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
+        for x in range(0, WIDTH, MAIN_TILE_SIZE):
+            pygame.draw.line(screen, (255, 255, 255), (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, MAIN_TILE_SIZE):
+            pygame.draw.line(screen, (255, 255, 255), (0, y), (WIDTH, y))
+
         pygame.display.flip()
         clock.tick(60)
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
